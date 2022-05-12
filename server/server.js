bbe292cad45bf80672cf44da6e310d61a49ff007
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-05-11 21:34:03 -0230

    Add client side

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
 });
 
 const app = express();
@@ -20,11 +19,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+
+
 
 db.once('open', () => {
     app.listen(PORT, () => {
